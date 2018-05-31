dc8b298e1c204e052a9de3ce11cda99973d05b70
nickkorolevv <37769528+nickkorolevv@users.noreply.github.com>
2018-05-31 23:27:22 +0300

    get open issues in another function

--- github_trending.py
+++ github_trending.py
@@ -1,9 +1,10 @@
 import requests
 import datetime
+from collections import defaultdict
 
 
 def date_number_of_days_ago(days_number):
-    return str(datetime.date.today() - datetime.timedelta(days=days_number))
+    return datetime.date.today() - datetime.timedelta(days=days_number)
 
 
 def get_trending_repositories(top_size, days_number):
@@ -11,21 +12,40 @@ def get_trending_repositories(top_size, days_number):
     date = "{}{}".format("created:>=", days_number)
     parameters = {'q': date, 'sort': 'stars', 'order': 'desc'}
     response_from_github = requests.get(url, params=parameters).json()
-    top_reps = response_from_github["items"][:top_size]
-    return top_reps
+    top_repos = response_from_github["items"][:top_size]
+    return top_repos
 
 
-def print_top_reps(top_reps):
-    for rep in top_reps:
-        print("Ссылка: {}, звезды: {}, открытые задачи:{} ".format(
+def get_top_repos_with_issues(top_repos):
+    issues_dict = defaultdict(list)
+    for rep in top_repos:
+        issues_url = (rep["issues_url"].rstrip("{/number}"))
+        parameters = {"state": "open"}
+        response_from_gihub = requests.get(
+            issues_url,
+            params=parameters)\
+            .json()
+        issues_dict[(
             rep["html_url"],
-            rep["stargazers_count"],
-            rep["open_issues_count"])
+            rep["stargazers_count"]
+        )].append(len(response_from_gihub))
+    return issues_dict
+
+
+def print_top_repos(top_repos_with_open_issues):
+    for top_rep, issues in top_repos_with_open_issues.items():
+        print(
+            "Ссылка: {}, звезды: {}, открытые вопросы: {}".format(
+                top_rep[0],
+                top_rep[1],
+                issues[0])
         )
+
 
 if __name__ == "__main__":
     days_number = 7
-    number_of_top_reps = 20
+    number_of_top_repos = 20
     days = date_number_of_days_ago(days_number)
-    top_reps = get_trending_repositories(number_of_top_reps, days)
-    print_top_reps(top_reps)
+    top_repos = get_trending_repositories(number_of_top_repos, days)
+    top_repos_with_open_issues = get_top_repos_with_issues(top_repos)
+    print_top_repos(top_repos_with_open_issues)
